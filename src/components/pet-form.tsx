01b199b7412b0badf32fe67016ffae416e4b5f0a
Louka <louka.cipolat@yahoo.fr>
2024-04-24 16:47:31 +0100

    react hook form + validation

--- src/components/pet-form.tsx
+++ src/components/pet-form.tsx
@@ -5,26 +5,45 @@ import { Input } from "./ui/input";
 import { Label } from "./ui/label";
 import { Textarea } from "./ui/textarea";
 import PetFormBtn from "./pet-form-btn";
+import { useForm } from "react-hook-form";
 
 type PetFormProps = {
   actionType: "add" | "edit";
   onSubmitForm: () => void;
 };
 
+type TPetFrom = {
+  name: string;
+  ownerName: string;
+  imageUrl: string;
+  age: number;
+  notes: string;
+};
+
 export default function PetForm({ actionType, onSubmitForm }: PetFormProps) {
   const { selectedPet, handleAddPet, handleEditPet } = usePetContext();
+
+  const {
+    register,
+    trigger,
+    formState: { errors },
+  } = useForm<TPetFrom>();
 
   return (
     <form
       action={async (formData) => {
+        const result = await trigger(); // trigger validation
+        if (!result) return;
 
-        onSubmitForm();// close the modal before adding to database because we use useOptimistic hook to update the UI instantly. React uptades states synchronously, so we need to force the Dialog to close before updating the other state (optimistic update) by using flushSync.
+        onSubmitForm(); // close the modal before adding to database because we use useOptimistic hook to update the UI instantly. React uptades states synchronously, so we need to force the Dialog to close before updating the other state (optimistic update) by using flushSync.
 
         //create shape for formData
         const petData = {
           name: formData.get("name") as string,
           ownerName: formData.get("ownerName") as string,
-          imageUrl: formData.get("imageUrl") as string || "https://bytegrad.com/course-assets/react-nextjs/pet-placeholder.png",
+          imageUrl:
+            (formData.get("imageUrl") as string) ||
+            "https://bytegrad.com/course-assets/react-nextjs/pet-placeholder.png",
           age: Number(formData.get("age")),
           notes: formData.get("notes") as string,
         };
@@ -34,7 +53,6 @@ export default function PetForm({ actionType, onSubmitForm }: PetFormProps) {
         } else if (actionType === "edit") {
           await handleEditPet(selectedPet!.id, petData);
         }
-      
       }}
       className="flex flex-col"
     >
@@ -42,56 +60,64 @@ export default function PetForm({ actionType, onSubmitForm }: PetFormProps) {
         <div className="space-y-1">
           <Label htmlFor="name">Name</Label>
           <Input
-            type="text"
             id="name"
-            name="name"
-            required
-            defaultValue={actionType === "edit" ? selectedPet?.name : ""}
+            {...register("name", {
+              required: "Name is required",
+              minLength: {
+                value: 3,
+                message: "Name must have at least 3 characters",
+              },
+              maxLength: {
+                value: 20,
+                message: "Name must have at most 20 characters",
+              },
+            })}
           />
+          {errors.name && <p className="text-red-500">{errors.name.message}</p>}
         </div>
 
         <div className="space-y-1">
           <Label htmlFor="ownerName" className="capitalize">
             Owner name
           </Label>
-          <Input
-            type="text"
-            id="ownerName"
-            name="ownerName"
-            required
-            defaultValue={actionType === "edit" ? selectedPet?.ownerName : ""}
-          />
+          <Input id="ownerName" {...register("ownerName",{
+            required: "Owner name is required",
+            minLength: {
+              value: 3,
+              message: "Owner name must have at least 3 characters",
+            },
+            maxLength: {
+              value: 20,
+              message: "Owner name must have at most 20 characters",
+            },
+          
+          })} />
+          {errors.ownerName && (
+            <p className="text-red-500">{errors.ownerName.message}</p>
+          )}
         </div>
 
         <div className="space-y-1">
           <Label htmlFor="imageUrl">Image URL</Label>
-          <Input
-            type="text"
-            id="imageUrl"
-            name="imageUrl"
-            defaultValue={actionType === "edit" ? selectedPet?.imageUrl : ""}
-          />
+          <Input id="imageUrl" {...register("imageUrl")} />
+          {errors.imageUrl && (
+            <p className="text-red-500">{errors.imageUrl.message}</p>
+          )}
         </div>
         <div className="space-y-1">
           <Label htmlFor="age">Age</Label>
-          <Input
-            type="number"
-            id="age"
-            name="age"
-            required
-            defaultValue={actionType === "edit" ? selectedPet?.age : ""}
-          />
+          <Input id="age" {...register("age",
+            
+          )} />
+          {errors.age && <p className="text-red-500">{errors.age.message}</p>}
         </div>
 
         <div className="space-y-1">
           <Label htmlFor="notes">Notes</Label>
-          <Textarea
-            id="notes"
-            rows={3}
-            name="notes"
-            required
-            defaultValue={actionType === "edit" ? selectedPet?.notes : ""}
-          />
+          <Textarea id="notes" {...register("notes")} />
+          {errors.notes && (
+            <p className="text-red-500">{errors.notes.message}</p>
+          )}
         </div>
       </div>
 
