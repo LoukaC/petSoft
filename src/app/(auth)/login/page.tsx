e9e14be81669dfef276f3f2f33da0df0fea7e2a0
Louka <louka.cipolat@yahoo.fr>
2024-04-25 09:51:53 +0100

    login sign up UI

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,5 +1,19 @@
+import AuthForm from "@/components/auth-form";
+import H1 from "@/components/h1";
+import Link from "next/link";
 import React from "react";
 
 export default function Page() {
-  return <div>Login</div>;
+  return (
+    <main>
+      <H1 className="text-center mb-5"> Log in</H1>
+      <AuthForm />
+      <p className="mt-6 text-sm text-zinc-500">
+        No account yet? {""}
+        <Link href="/signup" className="font-medium underline">
+          Sign up
+        </Link>
+      </p>
+    </main>
+  );
 }
--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,5 +1,19 @@
+import AuthForm from "@/components/auth-form";
+import H1 from "@/components/h1";
+import Link from "next/link";
 import React from "react";
 
 export default function Page() {
-  return <div>Signup</div>;
+  return (
+    <main>
+      <H1 className="text-center mb-5"> Sign Up</H1>
+      <AuthForm />
+      <p className="mt-6 text-sm text-zinc-500">
+        Already have an account yet? {""}
+        <Link href="/login" className="font-medium underline">
+          Log In
+        </Link>
+      </p>
+    </main>
+  );
 }
