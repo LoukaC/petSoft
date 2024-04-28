9a02f0b8e3f8f5f017a805dbb22226c6421ad984
Louka <louka.cipolat@yahoo.fr>
2024-04-28 16:37:12 +0100

    loading for logOut

--- src/components/sign-out-btn.tsx
+++ src/components/sign-out-btn.tsx
@@ -1,12 +1,17 @@
 "use client";
 import { logOut } from "@/actions/actions";
 import { Button } from "./ui/button";
+import { useTransition } from "react";
 
 export default function SignOutBtn() {
+  const [isPending, startTransition] = useTransition();
   return (
     <Button
+      disabled={isPending}
       onClick={async () => {
-        await logOut();
+        startTransition(async () => {
+          await logOut();
+        });
       }}
     >
       Log out
