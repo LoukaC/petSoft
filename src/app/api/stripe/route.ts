f4afd2fef119a6e6b04210fd2cfea2bdee096dec
Louka <louka.cipolat@yahoo.fr>
2024-04-29 20:35:01 +0100

    api/stripe route

--- src/app/api/stripe/route.ts
+++ src/app/api/stripe/route.ts
@@ -3,14 +3,14 @@ import prisma from "@/lib/db";
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 export async function POST(request: Request) {
-  const data = await request.json();
+  const body = await request.text();
   const stripeSignature = request.headers.get("Stripe-Signature");
 
   // verify webhook comes from stripe
   let event;
   try {
     event = stripe.webhooks.constructEvent(
-      data,
+      body,
       stripeSignature,
       process.env.STRIPE_WEBHOOK_SECRET
     );
@@ -24,7 +24,7 @@ export async function POST(request: Request) {
     case "checkout.session.completed":
       await prisma.user.update({
         where: {
-          email: data.data.object.customer_email,
+          email: event.data.object.customer_email,
         },
         data: {
           hasAccess: true,
